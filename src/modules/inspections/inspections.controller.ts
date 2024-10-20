ddd1a3e6cecce4e27fd268d3557f84e8f422ba1e
valens200 <valensniyonsenga2003@gmail.com>
2024-10-20 12:19:55 +0200

    feat: added report retrieval

--- src/modules/inspections/inspections.controller.ts
+++ src/modules/inspections/inspections.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Req, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseFilters,
+} from '@nestjs/common';
 import { InspectionsService } from './inspections.service';
 import { ApiResponse } from 'src/common/payload/ApiResponse';
 import { CreateInspectionDTO } from 'src/common/dtos/inspections/create-inspection.dto';
@@ -7,6 +15,7 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CustomExceptionFilter } from 'src/exceptions/CustomExceptionFilter';
 import { CreateInspectionPlanDTO } from 'src/common/dtos/inspections/create-inspection-plan.dto';
 import { Public } from 'src/decorators/public.decorator';
+import { UUID } from 'crypto';
 
 @Controller('inspections')
 @ApiTags('inspections')
@@ -28,5 +37,45 @@ export class InspectionsController {
     @Req() request: Request,
   ): Promise<ApiResponse> {
     return this.inspectionsService.createInspectionPlan(request, dto);
+  }
+
+  @Get('plan/:planId')
+  @Public()
+  async getInspectionPlan(@Param('planId') planId: UUID): Promise<ApiResponse> {
+    return new ApiResponse(
+      true,
+      'The inspection plan was retrieved successfully',
+      await this.inspectionsService.getInspectionPlan(planId),
+    );
+  }
+  @Get('categories/:categoryId')
+  @Public()
+  async getCategory(
+    @Param('categoryId') categoryId: UUID,
+  ): Promise<ApiResponse> {
+    return new ApiResponse(
+      true,
+      'The category was retrieved successfully',
+      await this.inspectionsService.getCategory(categoryId),
+    );
+  }
+
+  @Get('categories/plan-id/:planId')
+  @Public()
+  async getCategoriesInspectionPlan(
+    @Param('planId') planId: UUID,
+  ): Promise<ApiResponse> {
+    return new ApiResponse(
+      true,
+      'Categories were retrieved successfuly',
+      await this.inspectionsService.getCategoriesInspectionPlan(planId),
+    );
+  }
+  @Get('records/category-id/:categoryId')
+  @Public()
+  async getRecordsByCategory(
+    @Param('categoryId') categoryId: UUID,
+  ): Promise<ApiResponse> {
+    return this.inspectionsService.getRecordsByCategory(categoryId);
   }
 }
